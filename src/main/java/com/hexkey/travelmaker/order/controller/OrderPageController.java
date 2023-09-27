acd4b599e6bd7bb3cbb96b983fa3f8a325f0620a
kingnongmeel <kymtlfgdh7@naver.com>
2023-09-27 09:14:16 +0900

    #4 지도 맵 구현
    세부적인 꾸미기는 다시 해야합니다.

---
+++ src/main/java/com/hexkey/travelmaker/order/controller/OrderPageController.java
@@ -0,0 +1,12 @@
+package com.hexkey.travelmaker.order.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class OrderPageController {
+
+    @GetMapping("/order/page")
+    public void orderPage () {}
+
+}
