d1bf75738d76ab81c2ad7e825fcd234eed5cee8e
kingnongmeel <125981939+kingnongmeel@users.noreply.github.com>
2023-10-04 02:04:22 +0900

    #9-orderPage/Pay 10.04

--- src/main/resources/static/order/orderpage.js
+++ src/main/resources/static/order/orderpage.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    // 화살표 클릭 시 내용 숨김
+    ///////// 화살표 클릭 시 내용 숨김////////////////////
     const $ship_h2 = document.getElementById("ship_h2");
     const $ship_body = document.getElementById("ship_body");
     const $ship_h2_span = document.getElementById("ship_h2_span")
@@ -9,11 +9,11 @@ window.onload = function() {
     $ship_h2.addEventListener("click", function() {
 
         if (rotated) {
-            $ship_h2_span.style.transform = 'rotate(135deg)'; // 원래 각도로 회전
+            $ship_h2_span.style.transform = 'rotate(315deg)'; // 원래 각도로 회전
             $ship_body.style.display = "inline";
 
         } else {
-            $ship_h2_span.style.transform = 'rotate(315deg)'; // 새로운 각도로 회전
+            $ship_h2_span.style.transform = 'rotate(135deg)'; // 새로운 각도로 회전
             $ship_body.style.display = "none";
         }
 
@@ -30,11 +30,11 @@ window.onload = function() {
     $product_h2.addEventListener("click", function() {
 
         if (rotated2) {
-            $product_h2_span.style.transform = 'rotate(135deg)'; // 원래 각도로 회전
+            $product_h2_span.style.transform = 'rotate(315deg)'; // 원래 각도로 회전
             $product_body.style.display = "inline";
 
         } else {
-            $product_h2_span.style.transform = 'rotate(315deg)'; // 새로운 각도로 회전
+            $product_h2_span.style.transform = 'rotate(135deg)'; // 새로운 각도로 회전
             $product_body.style.display = "none";
         }
 
@@ -51,15 +51,83 @@ window.onload = function() {
     $discount_h2.addEventListener("click", function() {
 
         if (rotated3) {
-            $discount_h2_span.style.transform = 'rotate(135deg)'; // 원래 각도로 회전
+            $discount_h2_span.style.transform = 'rotate(315deg)'; // 원래 각도로 회전
             $discount_body.style.display = "inline";
 
         } else {
-            $discount_h2_span.style.transform = 'rotate(315deg)'; // 새로운 각도로 회전
+            $discount_h2_span.style.transform = 'rotate(135deg)'; // 새로운 각도로 회전
             $discount_body.style.display = "none";
         }
 
         rotated3 = !rotated3 ;
+
+    });
+
+    /////////////////////////////////////
+    const $pay_h2 = document.getElementById("pay_h2");
+    const $pay_body = document.getElementById("pay_body");
+    const $pay_h2_span = document.getElementById("pay_h2_span")
+
+    let rotated4 = false;
+
+    $pay_h2.addEventListener("click", function() {
+
+        if (rotated4) {
+            $pay_h2_span.style.transform = 'rotate(315deg)'; // 원래 각도로 회전
+            $pay_body.style.display = "inline";
+
+        } else {
+            $pay_h2_span.style.transform = 'rotate(135deg)'; // 새로운 각도로 회전
+            $pay_body.style.display = "none";
+        }
+
+        rotated4 = !rotated4 ;
+
+    });
+
+
+    const $pay_kind_h2 = document.getElementById("pay_kind_h2");
+    const $pay_kind_body = document.getElementById("pay_kind_body");
+    const $pay_kind_h2_span = document.getElementById("pay_kind_h2_span")
+
+    let rotated5 = false;
+
+    $pay_kind_h2.addEventListener("click", function() {
+
+        if (rotated5) {
+            $pay_kind_h2_span.style.transform = 'rotate(315deg)'; // 원래 각도로 회전
+            $pay_kind_body.style.display = "inline";
+
+        } else {
+            $pay_kind_h2_span.style.transform = 'rotate(135deg)'; // 새로운 각도로 회전
+            $pay_kind_body.style.display = "none";
+        }
+
+        rotated5 = !rotated5 ;
+
+    });
+
+
+
+
+    const $point_h2 = document.getElementById("point_h2");
+    const $point_body = document.getElementById("point_body");
+    const $point_h2_span = document.getElementById("point_h2_span")
+
+    let rotated6 = false;
+
+    $point_h2.addEventListener("click", function() {
+
+        if (rotated6) {
+            $point_h2_span.style.transform = 'rotate(315deg)'; // 원래 각도로 회전
+            $point_body.style.display = "inline";
+
+        } else {
+            $point_h2_span.style.transform = 'rotate(135deg)'; // 새로운 각도로 회전
+            $point_body.style.display = "none";
+        }
+
+        rotated6 = !rotated6 ;
 
     });
 
