8dbfd18d6f09bbe472ee23aa09c69895a3876fb4
alsry2325 <alsry2325@naver.com>
2023-10-04 15:39:46 +0900

    #2 임시

---
+++ src/main/java/com/hexkey/travelmaker/admin/dao/MemberMapper.java
@@ -0,0 +1,13 @@
+package com.hexkey.travelmaker.admin.dao;
+
+
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.Map;
+
+@Mapper
+public interface MemberMapper {
+
+    int selectTotalCount(Map<String, String> searchMap);
+
+}
