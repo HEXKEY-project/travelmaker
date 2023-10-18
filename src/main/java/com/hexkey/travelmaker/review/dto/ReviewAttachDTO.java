2e4d4486549a3ff3b1b88f0c3f962b102a3903e7
kim-mi-kyoung <alrud0605.notion@gmail.com>
2023-10-18 12:38:51 +0900

    #93 REVIEW 등록 기능 구현중

--- src/main/java/com/hexkey/travelmaker/review/dto/ReviewAttachDTO.java
+++ src/main/java/com/hexkey/travelmaker/review/dto/ReviewAttachDTO.java
@@ -6,4 +6,16 @@ import lombok.ToString;
 
 @Getter @Setter @ToString
 public class ReviewAttachDTO {
+
+    private Long fileCode;
+    private String fileType;
+    private String originalName;
+    private String savedName;
+    private String savePath;
+    private Long noticeCode;
+    private Long productSeq;
+    private Long reviewCode;
+    private Long qnaCode;
+    private String filePath;
+
 }
